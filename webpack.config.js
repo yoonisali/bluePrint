d35fdc7b1064dae0abc691d36f779ccdedaa1658
yoonisali <yoonismahamoudali@gmail.com>
2022-11-13 21:15:03 -0800

    add assests folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,52 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devServer: {                 
-    contentBase: './dist'      
-  },                           
-  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: "./dist",
+  },
+  devtool: "eval-source-map",
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin({
-      verbose: true
-    }), 
+      verbose: true,
+    }),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "Shape Tracker",
+      template: "./src/index.html",
+      inject: "body",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
 };
